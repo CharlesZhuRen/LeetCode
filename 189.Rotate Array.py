0055001f855fb07fea9ef4d70fea3577a7ee5c6f
CharlesZhu <charleszhuren@gmail.com>
2022-04-05 15:12:37 +0800

    added test cases in 189

--- 189.Rotate Array.py
+++ 189.Rotate Array.py
@@ -1,10 +1,18 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, nums: list[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
         k = k % n  # so that it won't do duplicate rotation
-        nums[:] = nums[n-k:] + nums[:n-k]
+        nums[:] = nums[n - k:] + nums[:n - k]
         # note that "nums[:] =" is different from "nums ="
         # the former works while the latter doesn't
+
+        print(nums)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.rotate([1, 2, 3], 1)
+    s.rotate([1, 2, 3], 4)
