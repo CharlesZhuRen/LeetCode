8f326b643141b88befe459ac8022e3c7fb3fa12b
CharlesZhuRen <charleszhuren@gmail.com>
2022-06-04 02:28:30 +0800

    added 1667

---
+++ SQL/1667. Fix Names in a Table.sql
@@ -0,0 +1,12 @@
+SELECT
+    user_id,
+    CONCAT(Upper(left(name,1)),Lower(substring(name,2))) AS name
+FROM
+    users
+ORDER BY
+    user_id
+;
+
+-- concat 2 parts of the raw name
+-- part 1: the first letter in upper case
+-- part 2: the rest letters in lower case
