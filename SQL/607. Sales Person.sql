428bcaffad400b13b9a0dd00f971548b2ea7996b
CharlesZhuRen <charleszhuren@gmail.com>
2022-06-09 19:29:41 +0800

    added 607

---
+++ SQL/607. Sales Person.sql
@@ -0,0 +1,13 @@
+SELECT
+    salesperson.name
+FROM
+    salesperson
+WHERE
+    sales_id NOT IN
+        (SELECT
+            sales_id
+         FROM
+            orders
+         WHERE
+            com_id = (SELECT com_id FROM company WHERE company.name='RED'))
+;
