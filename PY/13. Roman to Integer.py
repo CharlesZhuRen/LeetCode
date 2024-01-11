eaca2474b0bb5587d2a030dc83f5c0c34d82ee11
zr <charleszhuren@gmail.com>
2024-01-11 16:21:27 +0800

    Update 13

--- PY/13. Roman to Integer.py
+++ PY/13. Roman to Integer.py
@@ -1,25 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-
-        s2v = {'M': 1000,
-               'D': 500,
-               'C': 100,
-               'L': 50,
-               'X': 10,
-               'V': 5,
-               'I': 1}
+        # 从后往前遍历
+        # 若当前字符对应的值小于右边字符对应的值, 则减去当前字符对应的值, 反之加上
+        map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
         res = 0
-
-        while len(s) > 0:
-            if len(s) > 1:
-                if s2v[s[0]] < s2v[s[1]]:
-                    res -= s2v[s[0]]
-                else:
-                    res += s2v[s[0]]
+        n = len(s)
+        res += map[s[n - 1]]
+        for i in range(n - 2, -1, -1):
+            if map[s[i]] < map[s[i + 1]]:
+                res -= map[s[i]]
             else:
-                res += s2v[s[0]]
-            
-            s = s[1:]
+                res += map[s[i]]
 
         return res
