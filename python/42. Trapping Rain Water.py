5627c1ccf222b5675f87f5cf6b01368c1870c223
zr <charleszhuren@gmail.com>
2024-04-05 01:37:33 +0800

    update 42

--- python/42. Trapping Rain Water.py
+++ python/42. Trapping Rain Water.py
@@ -3,7 +3,7 @@ class Solution:
         # 对于下标i，下雨后水能到达的最大高度等于下标 i 两边的最大高度的最小值
         # 下标i处能接的雨水量等于下标 i 处的水能到达的最大高度减去height[i]
 
-        # 方法：动态规划
+        # 方法一：动态规划
 
         if not height:
             return 0
@@ -12,19 +12,25 @@ class Solution:
         # rightmax[i] = i位置右边的最大高度
 
         n = len(height)
-        leftMax = [height[0]] + [0] * (n - 1)  # 0位置的leftmax=height[0]
+
         # 从左到右记录leftmax
+        leftMax = [height[0]] + [0] * (n - 1)  # 0位置的leftmax=height[0]
+
         for i in range(1, n):
             leftMax[i] = max(leftMax[i - 1], height[i])
 
+        # 从右到左记录rightmax
         rightMax = [0] * (n - 1) + [height[n - 1]]  # n-1位置的rightmax=height[n-1]
+
         for i in range(n - 2, -1, -1):
             rightMax[i] = max(rightMax[i + 1], height[i])
 
-        ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+        # 为每个i计算能够接住的水的高度
+        ans = 0
+
+        for i in range(n):
+            ans += min(leftMax[i], rightMax[i]) - height[i]
 
         return ans
 
-        # 从右到左记录rightmax
 
-        # 为每个i计算能够接住的水的高度
