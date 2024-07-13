0a99317217bade7a5232eb15c603c67780499865
zr <charleszhuren@gmail.com>
2024-07-13 23:27:22 +0800

    update 88

--- leetcode-python/88. Merge Sorted Array.py
+++ leetcode-python/88. Merge Sorted Array.py
@@ -34,19 +34,36 @@ class Solution(object):
         # nums1[:] = sorted
 
         # 方法三: 逆向双指针, 避免覆盖原数组, 不用创建新数组
-        p1, p2 = m - 1, n - 1  # nums1和nums2的指针
-        tail = m + n - 1  # nums1的尾指针
-        while p1 >= 0 or p2 >= 0:
-            if p1 == -1:
-                nums1[tail] = nums2[p2]
-                p2 -= 1
-            elif p2 == -1:
-                nums1[tail] = nums1[p1]
-                p1 -= 1
-            elif nums1[p1] > nums2[p2]:
-                nums1[tail] = nums1[p1]
+        # p1, p2 = m - 1, n - 1  # nums1和nums2的指针
+        # tail = m + n - 1  # nums1的尾指针
+        # while p1 >= 0 or p2 >= 0:
+        #     if p1 == -1:
+        #         nums1[tail] = nums2[p2]
+        #         p2 -= 1
+        #     elif p2 == -1:
+        #         nums1[tail] = nums1[p1]
+        #         p1 -= 1
+        #     elif nums1[p1] > nums2[p2]:
+        #         nums1[tail] = nums1[p1]
+        #         p1 -= 1
+        #     else:
+        #         nums1[tail] = nums2[p2]
+        #         p2 -= 1
+        #     tail -= 1
+
+        # 方法三优化
+        p1 = m - 1  # 从后往前移动，指向nums1中的实值
+        p2 = n - 1  # 从后往前移动，指向nums2中的实质
+        p3 = m + n - 1  # 从后往前移动，指向nums1需要被赋值的位置
+        while p1 >= 0 and p2 >= 0:  # 有任何一个为-1，说明已经遍历完其中一个，应跳出循环
+            if nums1[p1] > nums2[p2]:  # 移动num1到nums1尾部
+                nums1[p3] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[tail] = nums2[p2]
+                nums1[p3] = nums2[p2]
                 p2 -= 1
-            tail -= 1
+
+            p3 -= 1
+
+        if p1 == -1:  # nums1先遍历结束，把nums2剩余的放到nums1的前面
+            nums1[:p2 + 1] = nums2[:p2 + 1]
